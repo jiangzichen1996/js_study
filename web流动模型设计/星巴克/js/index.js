57394698f0408a9ab291e55350f4ccad22d4ae3b
晨 <843909098@qq.com>
2017-08-05 19:14:24 +0800

    2017年8月5日学习内容

--- web流动模型设计/星巴克/js/index.js
+++ web流动模型设计/星巴克/js/index.js
@@ -1,8 +1,24 @@
 /**
- * Created by �� on 2017/8/2.
+ * Created by 晨 on 2017/8/2.
  */
 require(['jquery'],function($){
     $('#menu').on('click',function(){
+        $('#menu-list').css('width','270px');
+        $('#menu-mask').css('display','block');
+        $('#menu-list li').hover(function(e){
+            if($(this).parent().attr('class')=='nav-top'){
+                $(e.target).children('a').css('color','white');
+            }else{
+                $(e.target).children('a').andSelf().css('color','#b89f7a');
+            }
+        },function(e){
+            if($(this).parent().attr('class')=='nav-top'){
+                $(e.target).children('a').css('color','#b89f7a');
+            }else{
+                $(e.target).children('a').andSelf().css('color','white');
+            }
+        })
+
 
     })
 
