ac4416ea1d37cb68cc38d53be1768a18b77ab3e6
晨 <843909098@qq.com>
2017-10-29 21:52:15 +0800

    10月29日学习内容

--- 2017-秋季/webpack/src/index.js
+++ 2017-秋季/webpack/src/index.js
@@ -1,2 +1,16 @@
-import css from './css/index.css'
+import css from './css/index.css';
+import sass from './css/common.scss'
+// import $ from 'jquery';
 console.log('hello world123456sadassaf');
+let a=5;
+class Obj{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+
+};
+console.log(Obj);
+$('#div1').html('123456');
+let json = require('../author.json');
+console.log(json)
